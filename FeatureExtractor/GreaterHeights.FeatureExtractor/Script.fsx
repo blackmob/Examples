46a56427556a0b4b1d30caae219982d5b56313be
Ben Blackmore <ben.blackmore@greaterheights.onmicrosoft.com>
2017-02-13 14:22:00 +0000

    Fix refs

--- FeatureExtractor/GreaterHeights.FeatureExtractor/Script.fsx
+++ FeatureExtractor/GreaterHeights.FeatureExtractor/Script.fsx
@@ -1,4 +1,7 @@
-#r "C:\repos\Examples\FeatureExtractor\packages\EmguCV\lib\net30\Emgu.CV.UI.dll"
+#r "C:/repos/Examples/FeatureExtractor/packages/EmguCV/lib/net30/Emgu.CV.UI.dll"
+#r "C:/repos/Examples/FeatureExtractor/packages/EmguCV/lib/net30/Emgu.CV.UI.gl.dll"
+#r "C:/repos/Examples/FeatureExtractor/packages/EmguCV/lib/net30/Emgu.CV.World.dll"
+
 open System
 open System.IO
 open System.Drawing
