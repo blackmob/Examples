5a240710845b4b8870b55432b81efaad70d91e30
Ben Blackmore <ben.blackmore@greaterheights.onmicrosoft.com>
2017-03-02 08:48:44 +0000

    Remove project
    remove

--- MixcloudExplorer/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import cityReducer from './cityReducer';
-import {combineReducers} from 'redux';
-import showReducer from './showReducer';
-
-const rootReducer = combineReducers({city : cityReducer, show : showReducer });
-export default rootReducer;
-
